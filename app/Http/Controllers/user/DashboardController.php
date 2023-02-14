2a9ae205789050826c61af731fe111a5db502ed2
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2023-02-14 23:49:25 +0700

    feat: add session user

--- app/Http/Controllers/user/DashboardController.php
+++ app/Http/Controllers/user/DashboardController.php
@@ -14,28 +14,40 @@ class DashboardController extends Controller
         $data["login"] = false;
         return view("user.pages.home", $data);
     }
+    
     public function categories()
     {
         $data["navbarType"] = "side";
         $data["login"] = false;
         return view("user.pages.category", $data);
     }
+    
     public function albums()
     {
         $data["navbarType"] = "side";
         $data["login"] = false;
         return view("user.pages.album", $data);
     }
+    
     public function artist()
     {
         $data["navbarType"] = "side";
         $data["login"] = false;
         return view("user.pages.artist", $data);
     }
+    
     public function profile()
     {
         $data["navbarType"] = "side";
         $data["login"] = false;
+        $data["session"] = session()->all();
         return view("user.pages.profile", $data);
+    }
+    
+    public function getUUID()
+    {
+        $getUUID = DB::select("SELECT uuid() as uuid_value");
+        $uuid = $getUUID[0]->uuid_value;
+        return $uuid;
     }
 }
