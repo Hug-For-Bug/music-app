d1d7bb4f01e3f7b9edbcca6100e63ea7b871c2d4
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2023-02-22 00:21:45 +0700

    feat: welcome email view

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+<body>
+    <h1>Welcome to music app!</h1>
+    <p>Thanks for signing up. We hope you enjoy our app!</p>
+</body>
+</html>
