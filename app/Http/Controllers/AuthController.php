0110c90840ce07bf2417172b45d82b11fe08af7a
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2023-08-02 00:19:42 +0700

    feat: improve auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    const ROLE_USER = 'User';
+    const PLAN_FREE = 'Free';
+    const REDIRECT_ADMIN = 'administrator';
+    const REDIRECT_USER = 'user';
+
     public function index()
     {
         $data["navbarType"] = "top";
@@ -19,31 +23,28 @@ class AuthController extends Controller
         return view("login", $data);
     }
 
-    public function login(Request $req)
+    public function login(Request $request)
     {
-        if (!Auth::attempt(["email" => $req->email, "password" => $req->password])) {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
             return response()->json([
                 "success" => false,
-                "message" => "Email or password you entered is incorrect!"
+                "message" => "Email or password you entered is incorrect!",
             ]);
-        } else {
-            $user = Auth::user();
-            if ($user->id_role == 1) {
-                return response()->json([
-                    "success" => true,
-                    "message" => "Login Success",
-                    "user" => $user,
-                    "redirect" => "administrator"
-                ]);
-            } else {
-                return response()->json([
-                    "success" => true,
-                    "message" => "Login Success",
-                    "user" => $user,
-                    "redirect" => "user"
-                ]);
-            }
         }
+
+        $user = Auth::user();
+        $redirect = $user->id_role == 1 ? self::REDIRECT_ADMIN : self::REDIRECT_USER;
+        return response()->json([
+            "success" => true,
+            "message" => "Login Success",
+            "user" => $user,
+            "redirect" => $redirect,
+        ]);
     }
 
     public function register()
@@ -55,56 +56,60 @@ class AuthController extends Controller
         return view("register", $data);
     }
 
-    public function postRegister(Request $req)
+    public function postRegister(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|unique:users,phone',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = $this->createUser($request->all());
+
+        return response()->json([
+            "success" => true,
+            "message" => "User has been registered!",
+        ]);
+    }
+
+    private function createUser(array $data)
     {
         date_default_timezone_set('Asia/Jakarta');
-        $getUUID = DB::select("SELECT uuid() as uuid");
-        $uuid = $getUUID[0]->uuid;
+        $uuid = uniqid();
         $general = $this->getAdditionalInfo();
 
-        $email = $req->email;
-        $phone = $req->phone;
-        $checkEmail = DB::select("SELECT count(*) as total FROM users WHERE email = '$email'");
-        if ($checkEmail[0]->total != 0) {
-            return response()->json([
-                "success" => false,
-                "message" => "Email has been used, choose other email!"
-            ]);
-        }
-        $checkPhone = DB::select("SELECT count(*) as total FROM users WHERE phone = '$phone'");
-        if ($checkPhone[0]->total != 0) {
-            return response()->json([
-                "success" => false,
-                "message" => "Phone has been used, choose other phone number!"
-            ]);
-        }
-
-        User::create([
+        return User::create([
             "id" => $uuid,
-            "name" => $req->name,
-            "email" => $email,
-            "phone" => $phone,
-            "password" => Hash::make($req->password),
+            "name" => $data['name'],
+            "email" => $data['email'],
+            "phone" => $data['phone'],
+            "password" => Hash::make($data['password']),
             "id_plan" => $general['id_plan'],
             "id_role" => $general['id_role'],
             "verified_status" => "1",
-            "verified_at" => date("Y-m-d H:i:s")
+            "verified_at" => date("Y-m-d H:i:s"),
         ]);
-        return response()->json([
-            "success" => true,
-            "message" => "User has been registered!"
-        ]);
+    }
+
+    private function checkEmailAvailability($email)
+    {
+        return User::where('email', $email)->exists();
+    }
+
+    private function checkPhoneAvailability($phone)
+    {
+        return User::where('phone', $phone)->exists();
     }
 
     public function getAdditionalInfo()
     {
-        $user = DB::select("SELECT * FROM roles WHERE role_name = 'User'");
-        $free = DB::select("SELECT * FROM plans WHERE plan_name = 'Free'");
-        $data = [
-            'id_role' => $user[0]->id,
-            'id_plan' => $free[0]->id
+        $userRole = DB::table('roles')->where('role_name', self::ROLE_USER)->first();
+        $freePlan = DB::table('plans')->where('plan_name', self::PLAN_FREE)->first();
+        return [
+            'id_role' => $userRole->id,
+            'id_plan' => $freePlan->id,
         ];
-        return $data;
     }
 
     public function logout()
