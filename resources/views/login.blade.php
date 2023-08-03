4e75e8d78a65edc6d494722fa5b58b1c61dfd19f
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2023-08-03 23:21:31 +0700

    feat: improve login blade

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,64 +1,57 @@
 @extends('user.layouts.default')
 @section('content')
 <main>
-    <div id="primary">
+    <section id="primary">
         <div class="container">
-            <div class="text-center s-14 l-s-2 my-5">
-                <a class="my-5" href="index.html">
-
+            <header class="text-center my-5">
+                <a href="index.html">
                     <span>RECORD</span>
-
                 </a>
-            </div>
+            </header>
             <div class="row">
                 <div class="col-md-10 mx-md-auto">
                     <div class="mt-5">
                         <div class="row grid">
                             <div class="col-md-7 card p-5">
-                                <form class="form-material" id="formLogin" method="GET" action="{{ url('signIn') }}">
+                                <form class="form-material" id="formLogin" method="POST" action="{{ url('signIn') }}">
                                     @csrf
                                     <!-- Input -->
                                     <div class="body">
                                         <div class="form-group form-float">
+                                            <label for="email" class="form-label">Email</label>
                                             <div class="form-line">
                                                 <input type="text" id="email" name="email" class="form-control">
-                                                <label class="form-label">Email</label>
                                             </div>
                                         </div>
                                         <div class="form-group form-float">
+                                            <label for="password" class="form-label">Password</label>
                                             <div class="form-line">
                                                 <input type="password" id="password" name="password" class="form-control">
-                                                <label class="form-label">Password</label>
                                             </div>
                                         </div>
-
-
                                     </div>
                                     <!-- #END# Input -->
                                     <input type="submit" id="btnLogin" class="btn btn-primary btn-sm pl-4 pr-4 p-2" value="Log In">
 
                                     <div class="pt-5">
-                                        <small><a href="#">Forgot Password?</a></small>
+                                        <small><a href="{{ url('password/reset') }}">Forgot Password?</a></small>
                                     </div>
                                 </form>
                             </div>
-                            <div class="col-md-5  p-5">
+                            <div class="col-md-5 p-5">
                                 <h1 class="mt-3 font-weight-lighter">Are You New Here?</h1>
-
                                 <div class="pt-3 mb-5">
                                     <p>Lorem ipsum dolor sit amet, sapiente tenetur ut, veritatis.</p>
                                 </div>
-                                <a href="{{ url('register') }}"><input type="submit" value="Create Account" class="btn btn-outline-primary s-14 pl-4 pr-4"></a>
-
-
-
+                                <a href="{{ url('register') }}">
+                                    <input type="submit" value="Create Account" class="btn btn-outline-primary s-14 pl-4 pr-4">
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </main>
-<!--@Page Content-->
 @endsection
