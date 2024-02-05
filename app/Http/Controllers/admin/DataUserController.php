f5d4e172113b36484fed1b5c5b0d766d479408d0
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2024-02-05 23:55:05 +0700

    feat: adjust plan model

--- app/Http/Controllers/admin/DataUserController.php
+++ app/Http/Controllers/admin/DataUserController.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Models\Plan;
 use App\Models\Role;
 use Illuminate\Support\Facades\Hash;
 
